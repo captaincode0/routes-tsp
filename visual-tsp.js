88079b4413123dd939ae432220ef48a28e78b096
captaincode0 <developerdiego0@gmail.com>
2018-11-07 10:50:00 -0600

    TSP/config Updated heroku configuration

--- visual-tsp.js
+++ visual-tsp.js
@@ -34,7 +34,7 @@ var MSG_SEND_INTERVAL_MIDDLE = 500;
 var MSG_SEND_INTERVAL_HIGH = 1000;
 
 var CANVAS_DIMENSION_PXLS = 700;
-var DEFAULT_WS_ADDRESS = "ws://"+document.location.host+"9999/tsp";
+var DEFAULT_WS_ADDRESS = "wss://"+document.location.host+"9999/tsp";
 
 var CB_ID_1 = 0; // checkbox identifier
 var CB_ID_2 = 1; // checkbox identifier
